33747cfa1811c2dc4186ff1ec08789c28936cd0f
testsfortests <158587957+testsfortests@users.noreply.github.com>
2024-03-06 15:03:58 +0000

    increment cell value

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
-import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from dotenv import load_dotenv
-dotenv_path = '/.env'
-load_dotenv(dotenv_path)
-# load_dotenv()
 
+# /etc/secrets/credentials.json
 # Define FastAPI app
 app = FastAPI()
 
@@ -16,52 +12,7 @@ app = FastAPI()
 class SheetData(BaseModel):
     cell_value: int
 
-# Google Sheets credentials file (JSON file downloaded from Google Cloud Console)
-# Retrieve the JSON string from the environment variable
-# cred_json_string = os.getenv('GOOGLE_CREDENTIALS_JSON')
 
-# json_keyfile_data = {
-#     "type": os.getenv('type'),
-#     "project_id": os.getenv('project_id'),
-#     "private_key_id":os.getenv('private_key_id'),
-#     "private_key":os.getenv('private_key'),
-#     "client_email": os.getenv('client_email'),
-#     "client_id": os.getenv('client_id'),
-#     "auth_uri": os.getenv('auth_uri'),
-#     "token_uri": os.getenv('token_uri'),
-#     "auth_provider_x509_cert_url": os.getenv('auth_provider_x509_cert_url'),
-#     "client_x509_cert_url": os.getenv('client_x509_cert_url'),
-#     "universe_domain":os.getenv('universe_domain')
-#   }
-
-# default_values = {
-#     "type": "service_account",
-#     "project_id": "your_default_project_id",
-#     "private_key_id": "your_default_private_key_id",
-#     "private_key": "your_default_private_key",
-#     "client_email": "your_default_client_email",
-#     "client_id": "your_default_client_id",
-#     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-#     "token_uri": "https://oauth2.googleapis.com/token",
-#     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-#     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/your_client_email%40your_project_id.iam.gserviceaccount.com",
-#     "universe_domain": "default_universe_domain"
-# }
-# json_keyfile_data = {
-#     "type": os.getenv('type', default_values['type']),
-#     "project_id": os.getenv('project_id', default_values['project_id']),
-#     "private_key_id": os.getenv('private_key_id', default_values['private_key_id']),
-#     "private_key": os.getenv('private_key', default_values['private_key']),
-#     "client_email": os.getenv('client_email', default_values['client_email']),
-#     "client_id": os.getenv('client_id', default_values['client_id']),
-#     "auth_uri": os.getenv('auth_uri', default_values['auth_uri']),
-#     "token_uri": os.getenv('token_uri', default_values['token_uri']),
-#     "auth_provider_x509_cert_url": os.getenv('auth_provider_x509_cert_url', default_values['auth_provider_x509_cert_url']),
-#     "client_x509_cert_url": os.getenv('client_x509_cert_url', default_values['client_x509_cert_url']),
-#     "universe_domain": os.getenv('universe_domain', default_values['universe_domain'])
-# }
-# Parse the JSON string back into a Python dictionary
-# credentials_file = json.loads(cred_json_string)
 # Google Sheets API scope
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
@@ -72,9 +23,7 @@ sheet_key = '178Jr7nTOk24Iac-f3-OdXz9vWgmLFp1D419B0w00Meg'
 sheet_name = 'RENDER'
 
 # Authenticate with Google Sheets API
-# creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, scope)
-# creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_file, scope)
-creds = ServiceAccountCredentials.from_json_keyfile_dict(json_keyfile_data, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("/etc/secrets/credentials.json", scope)
 
 client = gspread.authorize(creds)
 
